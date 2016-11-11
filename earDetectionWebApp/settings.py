c29c6bb6c5d1c2c46c61f10619b77103a2883456
Martin Tomik <mtomik@live.com>
2016-11-11 19:37:45 +0100

    fix

--- earDetectionWebApp/settings.py
+++ earDetectionWebApp/settings.py
@@ -127,6 +127,7 @@ LOGIN_REDIRECT_URL = '/earTrainer/'
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"),
                      '/home/tomik/git/earFront/static/')
 
--- earDetectionWebApp/urls.py
+++ earDetectionWebApp/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
     url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
     url(r'^logout/$', views.logout, {'next_page': '/login'}),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
