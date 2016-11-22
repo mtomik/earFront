6da086953f754f69d2d0f65da697059782a9626f
Martin Tomik <mtomik@live.com>
2016-11-22 23:26:21 +0100

    fix

--- earDetectionWebApp/settings.py
+++ earDetectionWebApp/settings.py
@@ -146,3 +146,12 @@ MEDIA_URL = 'media/'
 FILE_UPLOAD_TEMP_DIR = os.path.join(BASE_DIR,"media")
 
 
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Bratislava'
+
+
