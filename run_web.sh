32384733eacf8c6931cd263a43eed10003e9fb2a
Martin Tomik <mtomik@live.com>
2017-04-24 10:04:13 +0200

    rpi fix

--- run_web.sh
+++ run_web.sh
@@ -4,8 +4,8 @@
 sleep 10
 
 # migrate db, so we have the latest db schema
-python manage.py makemigrations && python manage.py migrate
+python3 manage.py makemigrations && python manage.py migrate
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('xtomik', 'xtomik@stuba.sk', 'abc123456')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('xtomik', 'xtomik@stuba.sk', 'abc123456')" | python3 manage.py shell
 # start development server on public ip interface, on port 8000
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8080
